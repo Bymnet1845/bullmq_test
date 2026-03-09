12da5a0f4413e20ccb6d08da157319561395da3a
Bymnet1845 <haraheri5robymnet1845@yahoo.co.jp>
2026-03-10 04:44:20 +0900

    なありさんの指摘を反映。

--- index.js
+++ index.js
@@ -1,54 +1,58 @@
 import { Queue, Worker } from "bullmq";
 import * as dotenv from "dotenv";
-import mysql from "mysql";
+import mysql from "mysql2/promise";
 
 dotenv.config();
 const MISSKEY_ACCESS_TOKEN = process.env.MISSKEY_ACCESS_TOKEN;
 
-const MYSQL_CONNECTION = mysql.createConnection({
+const mysqlPool = mysql.createPool({
 	host: process.env.MYSQL_HOST,
 	user: process.env.MYSQL_USER,
 	password: process.env.MYSQL_PASSWORD,
 	database: process.env.MYSQL_DATABASE,
 	supportBigNumbers: true,
-	bigNumberStrings: true
+	bigNumberStrings: true,
+	namedPlaceholders: true,
+	connectionLimit: 1,
 });
 
-const TEST_QUEUE = new Queue("test_2026-02-16-20-23");
+const TEST_QUEUE = new Queue("test_2026-03-10-04-34");
 
 const TEST_WORKER = new Worker(
-	"test_2026-02-16-20-23",
+	"test_2026-03-10-04-34",
 	async (job) => {
-		MYSQL_CONNECTION.query(`SELECT * FROM users WHERE id=${job.data.id};`, async (error, users, fields) => {
-			if (error) {
-				console.error(error);
-				return;
-			}
-
-			const RESULT = await getActivities(users[0].niconico_id);
-
-			for (let activity of RESULT.activities) {
+		try {
+			const [users, fields] = await mysqlPool.query(`SELECT * FROM users WHERE id=:id`, { id: job.data.id });
+			const result = await getActivities(users[0].niconico_id);
+			
+			for (let activity of result.activities) {
 				if (activity.id === users[0].last_acquired_activity_id) break;
 				await post(activity, users[0].misskey_host, users[0].misskey_access_token);
-			};
+			}
 
-			MYSQL_CONNECTION.query(`UPDATE users SET last_acquired_activity_id="${RESULT.activities[0].id}" WHERE id=${job.data.id};`, (error, results, fields) => { if (error) console.error(error); });
-		});
-	},
-	{ connection: { host: "localhost", port: 6379 }}
-);
-
-await TEST_QUEUE.obliterate().then(() => {
-	MYSQL_CONNECTION.query("SELECT id FROM users;", (error, users, fields) => {
-		if (error) {
+			await mysqlPool.query(`UPDATE users SET last_acquired_activity_id=:activityId WHERE id=:userId`, { activityId: result.activities[0].id, userId: job.data.id });
+		} catch (error) {
 			console.error(error);
 			return;
 		}
+	},
+	{ 
+		connection: { host: "localhost", port: 6379 },
+		limiter: { max: 1, duration: 1000, },
+	}
+);
+
+await TEST_QUEUE.obliterate({ force: true }).then(async () => {
+	try {
+		const [users] = await mysqlPool.query("SELECT id FROM users");
 
 		users.forEach((user) => {
-			TEST_QUEUE.add(`test_${user.id}_${Date.now()}`, { id: user.id }, { repeat: { every: 300000 }, connection: { host: "localhost", port: 6379 }});
+			TEST_QUEUE.add(`test_${user.id}_${Date.now()}_0`, { id: user.id }, { repeat: { every: 60000 }, connection: { host: "localhost", port: 6379 }});
 		});
-	});
+	} catch (error) {
+		console.error(error);
+		return;
+	}
 });
 
 async function getActivities(userId) {
@@ -73,7 +77,7 @@ async function post(activity, host, accessToken) {
 		body: JSON.stringify({
 			i: accessToken,
 			text: `テスト\n${activity.message.text}\n${activity.content.title} ${activity.content.url} #ニコニコ動画 #${activity.content.id}`,
-			visibility: "home"
+			visibility: "specified"
 		})
 	})
 }
